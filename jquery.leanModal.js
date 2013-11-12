a2efb05dd6543b07908b4ceb0589c97857a3c864
Mossuru777 <mossuru777@gmail.com>
2013-11-13 00:33:45 +0900

    add some feature and made function from some codes
    [add] constructor argument: called from element want to show modal (argument backward compatible)
    [add] init option: auto vertical centering of element (option backward compatible)
    [improve] check whether or not the insertion of overlay div block the duplicate
    [modify] split open modal codes, made 'open_modal' function

--- jquery.leanModal.js
+++ jquery.leanModal.js
@@ -1,60 +1,103 @@
 (function($){
     $.fn.extend({
 
-        leanModal: function(options) {
+        leanModal: function(options, isCalledFromTargetElement) {
 
             var defaults = {
               top: 100,
               overlay: 0.5,
-              closeButton: null
+              closeButton: null,
+              verticalCenter: false
             }
 
-            var overlay = $("<div id='lean_overlay'></div>");
-            $("body").append(overlay);
+            if(typeof $('body > div#lean_overlay').get(0) === 'undefined') {
+                var overlay = $("<div id='lean_overlay'></div>");
+                $("body").append(overlay);
+            }
+
             options =  $.extend(defaults, options);
+
+            if(typeof isCalledFromTargetElement === 'undefined') {
+                isCalledFromTargetElement = false;
+            }
 
             return this.each(function() {
                 var o = options;
 
-                $(this).click(function(e) {
-                    var modal_id = $(this).attr("href");
+                if(isCalledFromTargetElement == false) {
+                    $(this).click(function(e) {
+                        var modal_block = $($(this).attr("href"));
 
-                    $("#lean_overlay").click(function() {
-                        close_modal(modal_id);
+                        open_modal(modal_block);
+                        e.preventDefault();
                     });
+                } else {
+                    open_modal($(this));
+                }
 
-                    $(o.closeButton).click(function() {
-                        close_modal(modal_id);
-                    });
-
-                    var modal_height = $(modal_id).outerHeight(),
-                        modal_width = $(modal_id).outerWidth();
+                function open_modal(modal_block){
+                    var modal_height = modal_block.outerHeight(),
+                        modal_width = modal_block.outerWidth();
+                        modal_id = modal_block.attr('id');
 
                     $('#lean_overlay').css({ 'display' : 'block', opacity : 0 });
                     $('#lean_overlay').fadeTo(200,o.overlay);
 
-                    $(modal_id).css({
-                        'display' : 'block',
-                        'position' : 'fixed',
-                        'opacity' : 0,
-                        'z-index': 11000,
-                        'left' : 50 + '%',
-                        'margin-left' : -(modal_width/2) + "px",
-                        'top' : o.top + "px"
+                    if(o.verticalCenter) {
+                        var left = (modal_width <= document.documentElement.clientWidth) ? (document.documentElement.clientWidth - modal_width) / 2 + 'px' : '0px',
+                            top = (modal_height <= document.documentElement.clientHeight) ? (document.documentElement.clientHeight - modal_height) / 2 + 'px' : '0px';
+                        modal_block.replaceWith(
+                            $('<div id="lean_wrap"/>').css({
+                                'position' : 'absolute',
+                                'top' : 0,
+                                'right' : 0,
+                                'bottom' : 0,
+                                'left' : 0
+                            })
+                            .append(
+                                modal_block.clone(true,true).css({
+                                    'position' : 'absolute',
+                                    'opacity' : 0,
+                                    'z-index' : 11000,
+                                    'left' : left,
+                                    'top' : top
+                                })
+                            )
+                        );
+                    } else {
+                        modal_block.css({
+                            'display' : 'block',
+                            'position' : 'fixed',
+                            'opacity' : 0,
+                            'z-index': 11000,
+                            'left' : 50 + '%',
+                            'margin-left' : -(modal_width/2) + "px",
+                            'top' : o.top + "px"
+                        });
+                    }
+
+                    modal_block = $('#' + modal_id);
+
+                    $("#lean_overlay").click(function() {
+                        close_modal(modal_block);
                     });
 
-                    $(modal_id).fadeTo(200,1);
+                    $(o.closeButton).click(function() {
+                        close_modal(modal_block);
+                    });
 
-                    e.preventDefault();
+                    modal_block.fadeTo(200,1);
 
-                });
+                }
 
+                function close_modal(modal_block){
+                    $("#lean_overlay").fadeOut(200);
+                    modal_block.css({ 'display' : 'none' });
+                    if(typeof modal_block.parent('#lean_wrap').get(0) !== 'undefined') {
+                        modal_block.parent('#lean_wrap').replaceWith(modal_block.clone(true,true));
+                    }
+                }
             });
-
-            function close_modal(modal_id){
-                $("#lean_overlay").fadeOut(200);
-                $(modal_id).css({ 'display' : 'none' });
-            }
         }
     });
 })(jQuery);
